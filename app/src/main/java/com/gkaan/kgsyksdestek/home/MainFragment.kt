64f33fbdacb42ddcac38e994d35ff2e36c6f7f05
Kaan-GUL60 <gkaan783@gmail.com>
2023-11-11 22:01:40 +0300

    Gkaan version update donut chart libray updated

--- app/src/main/java/com/gkaan/kgsyksdestek/home/MainFragment.kt
+++ app/src/main/java/com/gkaan/kgsyksdestek/home/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.gkaan.kgsyksdestek.home
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build.VERSION_CODES.DONUT
 import android.os.Bundle
@@ -7,33 +8,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.db.williamchart.data.configuration.ChartConfiguration
 import com.gkaan.kgsyksdestek.R
 import com.gkaan.kgsyksdestek.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-    private lateinit var chartConfiguration: ChartConfiguration
-    private var _binding: FragmentMainBinding?=null
+    private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.apply {
-            donutChartView.donutColors= intArrayOf(
-                Color.parseColor("#FFFFFF"),Color.parseColor("#9EFFFFFF"),Color.parseColor("8DFFFFFF")
-            )
-            donutChartView.animation.duration = animationDuration
-            donutChartView.animate(donutSet)
-        }
-    }
 
-    companion object{
-        private val donutSet = listOf(
-            20F,
-            80F,
-            100F
-        )
-        private const val animationDuration = 1000L
     }
 
     override fun onCreateView(
@@ -41,12 +24,11 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding =FragmentMainBinding.inflate(inflater,container,false)
+        _binding = FragmentMainBinding.inflate(inflater,container, false)
         val view = binding.root
-
         return view
 
-    }
 
+    }
 
 }
